b1d579fb2e6d19c6fb42d9c4e5d4aaf3da334b5e
awal <awal.aaraf.92@gmail.com>
2025-05-03 21:28:08 +0600

    dev 1 tailwind css add krar pr final project

--- event_cnfig/settings.py
+++ event_cnfig/settings.py
@@ -14,7 +14,8 @@ SECRET_KEY = 'django-insecure-_j9ba#q4lndx+tf76_#fpfgm(zp)yu=i$_#9$qqq!qu@^60#ft
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS=['https://*.onrender.com','http://127.0.0.1:8000']
 
 
 # Application definition
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-      "./templates/**/*.html", // Templates at the project level
+      "./templates/**/*.html", // Templates at the project level or rot level 
       "./**/templates/**/*.html", // Templates inside apps
     ],
     theme: {
